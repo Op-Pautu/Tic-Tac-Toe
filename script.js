62ca02570fe87f6cbedfab823359b871e32e17d5
Op Pautu <pautuop@gmail.com>
2023-01-16 15:54:36 +0530

    add factory and module

--- script.js
+++ script.js
@@ -11,3 +11,46 @@ const Gameboard = {
       }
     }
   }
+
+  // Player factory function
+function createPlayer(symbol) {
+    return {
+      symbol: symbol,
+      win: false
+    }
+  }
+  
+  // Game flow module
+  const game = (function() {
+    let player1 = createPlayer("X");
+    let player2 = createPlayer("O");
+    let currentPlayer = player1;
+  
+    function render() {
+      Gameboard.render();
+    }
+    
+    function play(event) {
+      let cell = event.target;
+      if(!cell.innerHTML){
+          let index = cell.getAttribute("data-index");
+          Gameboard.board[index] = currentPlayer.symbol;
+          render();
+          checkWin();
+          switchPlayer();
+      }
+    }
+    function checkWin() {
+        // check for win conditions
+      }
+      
+      function switchPlayer() {
+        currentPlayer = (currentPlayer === player1) ? player2 : player1;
+      }
+    
+      return {
+        play: play,
+        render: render
+      }
+    })();
+    
